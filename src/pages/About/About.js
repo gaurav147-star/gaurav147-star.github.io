cd574018cf97663ee200cdffd07478f2000f7e1c
gaurav147-star <gauravgupta9158@gmail.com>
2023-06-12 09:22:36 +0530

    changes in projects and experience desc  and favico not working

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -24,19 +24,21 @@ const About = () => {
             </h2>
             <p className="text-[1.4rem] text-lg text-[#ff7700] text-justify font-semibold  italic md:not-italic">
               {" "}
-              I am Gaurav Gupta and am currently pursuing a B.Tech with an
+              I am Gaurav Gupta and I am currently pursuing a B.Tech with an
               emphasis in Information Technology at IIIT Bhopal.
             </p>
             <p className=" text-[1.4rem] text-lg text-textBase text-justify text-white	font-medium italic md:not-italic">
               {" "}
-              Besides being Club Secretary of the GNU/Linux Users Club IIIT
-              Bhopal, I am also a Web Development Team member of the CODAME Club
-              IIIT Bhopal. My knack for understanding and identifying trending
-              patterns and developments comes from my passion to learn. I am
-              fascinated by workplaces with positive and productive atmospheres,
-              as well as team morale. In turn, this motivates me to work hard
-              and put in extra effort every day as a loyal team member. I enjoy
-              listening to people's ideas and have good communication skills.
+              I am the web development lead of Codame Club at IIIT Bhopal.
+              Besides being the Club Secretary of the GNU/Linux Users Club at
+              IIIT Bhopal, I am also a member of the Web Development Team at
+              Codame Club IIIT Bhopal. My ability to understand and identify
+              trending patterns and developments comes from my passion for
+              learning. I am fascinated by workplaces with positive and
+              productive atmospheres, as well as strong team morale. This
+              motivates me to work hard and put in extra effort every day as a
+              dedicated team member. I enjoy listening to people's ideas and
+              possess good communication skills.
             </p>
             <a
               href="https://drive.google.com/file/d/1esY7IVAOfREdDpaaJNemXrRPgr1CPKmm/view?usp=sharing"
